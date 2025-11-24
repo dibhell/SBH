4cc1941ba839817439745716c8988991bf121efc
dibhell <dib.hell@gmail.com>
2025-11-24 13:42:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for GitHub Pages relative paths
+  base: '/visus/'', // Important for GitHub Pages relative paths
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   }
 });
+
+//import { defineConfig } from 'vite'
+//export default defineConfig({
+//  base: '/visus/',   // <--- NAZWA REPO
+//})
